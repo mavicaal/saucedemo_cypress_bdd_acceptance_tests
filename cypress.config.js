a507990a7b6e9a40365e02b0855a5fbea374bb37
Marco Calderon <115949217+mavicaal@users.noreply.github.com>
2022-10-24 09:31:32 -0600

    Installing cucumber dependency

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,57 @@
 const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+          fallback: {
+            crypto: false,
+            path: require.resolve("path-browserify"),
+          },
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+            {
+              test: /\.jsx?$/,
+              exclude: [/node_modules/],
+              use: [
+                {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ["@babel/preset-env"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
     chromeWebSecurity: false,
   },
 });
